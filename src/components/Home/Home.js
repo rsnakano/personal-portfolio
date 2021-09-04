2fd08d0dfafc65567b3091bb2f926a92f75cb3c7
rsnakano <rsnakano@up.edu.ph>
2021-09-04 13:29:12 +0800

    Implements particle animation

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,35 @@
+import Particles from 'react-particles-js'
 import './Home.css'
 import profpic from '../../resources/profpic.png'
 
 const Home = () => {
     return (
         <div className='container'>
+            <Particles className='particles' params={{
+                particles: {
+                    number: {
+                        value: 125,
+                        density: {
+                        enable: true,
+                        value_area: 950,
+                        }
+                    },
+                    size: {
+                        value: 3,
+                    },
+                    move: {
+                        speed: 1.5,
+                    }
+                },
+                interactivity: {
+                    events: {
+                        onhover: {
+                            enable: true,
+                            mode: "grab"
+                        }
+                    }
+                },
+            }} />
             <div className='main'>
                 <div className='main-box'>
                     <div>
@@ -16,7 +42,7 @@ const Home = () => {
             <div className='about'>
                 
             </div>
-        </div>
+         </div> // container end
     )
 }
 
